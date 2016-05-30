7fcc3d80da104f5942c4cffcc6b0046ecffefbd2
Adam Tootle <adamtootle@gmail.com>
2016-05-30 00:19:23 -0400

    Adding the status bar icon and basic window opening.

--- main.js
+++ main.js
@@ -6,14 +6,43 @@ const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 const globalShortcut = electron.globalShortcut;
+const Tray = electron.Tray;
+const Menu = electron.Menu;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow() {
+app.dock.hide();
+
+function didClickStatusBarIcon(ev, bounds) {
+
+  mainWindow.setBounds({
+    x: bounds.x,
+    y: bounds.y,
+    width: mainWindow.getBounds().width,
+    height: mainWindow.getBounds().height
+  });
+
+  if (mainWindow.isVisible()) {
+    mainWindow.hide();
+  } else {
+    mainWindow.show();
+  }
+}
+
+function createMainWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
+  mainWindow = new BrowserWindow({
+    width: 280,
+    height: 460,
+    frame: false,
+    movable: false,
+    resizable: false,
+    minimizable: false,
+    maximizable: false,
+    show: false
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
@@ -28,11 +57,33 @@ function createWindow() {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
+
+  mainWindow.on('blur', () => {
+    mainWindow.hide();
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', createWindow);
+// app.on('ready', createWindow);
+
+let statusBarIcon = null;
+
+function setupStatusBarIcon() {
+  statusBarIcon = new Tray('images/status-bar-icon.png');
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Item1', type: 'radio'},
+    {label: 'Item2', type: 'radio'},
+    {label: 'Item3', type: 'radio', checked: true},
+    {label: 'Item4', type: 'radio'}
+  ]);
+  statusBarIcon.setToolTip('This is my application.');
+  // appIcon.setContextMenu(contextMenu);
+  statusBarIcon.on('click', didClickStatusBarIcon);
+}
+
+app.on('ready', createMainWindow);
+app.on('ready', setupStatusBarIcon);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
