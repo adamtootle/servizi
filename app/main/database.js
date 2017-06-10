d0cffcd1310af555dddbbe91ba54a49f0beb8525
Adam Tootle <adamtootle@gmail.com>
2017-06-10 03:33:12 -0400

    Adding support for encrypting/decrypting stored oauth data in nedb.

--- app/main/database.js
+++ app/main/database.js
@@ -1,9 +1,22 @@
 const utils = require('./utils');
 const NeDB = require('nedb');
+const CryptoJS = require('crypto-js');
+const config = require('../../config');
 const electron = utils.isRenderer() ? require('electron').remote : require('electron');
 const app = electron.app;
 
 module.exports = new NeDB({
   filename: `${app.getPath('appData')}/Servizi/database`,
   autoload: true,
+  afterSerialization: (string) => {
+    if (string) {
+      return CryptoJS.AES.encrypt(string, config.dbSecret);
+    }
+  },
+  beforeDeserialization: (string) => {
+    if (string) {
+      const bytes = CryptoJS.AES.decrypt(string.toString(), config.dbSecret);
+      return bytes.toString(CryptoJS.enc.Utf8);
+    }
+  },
 });
